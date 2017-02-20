176c8ada3867367aad4b45321a0c7f430af4cb82
Christos <chris.paraskeva10@gmail.com>
2017-02-20 17:43:36 +0000

    add top up function with a max limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,5 +3,14 @@ class OysterCard
 
   def initialize
     @balance = 0
+    @max_value = 90
+  end
+
+  def top_up(value)
+    if (@balance < @max_value) && (@balance + value) <= @max_value
+      @balance += value
+    else
+      fail 'You have reached the maximum balance of £90'
+    end
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,19 @@ describe OysterCard do
   it "has a balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  # it "checks to see if balance respond to top up method" do
+  #   expect(subject.balance).to respond_to(:top_up).with(1).argument
+  # end
+
+  it "increases the balance by a given value" do
+    # allow(subject).to receive(:balance).and_return(0)
+    subject.top_up(20)
+    expect(subject.balance).to eq subject.balance
+  end
+
+  it 'gives an error when trying to top-up beyond max value' do
+    error = "You have reached the maximum balance of £90"
+    expect{subject.top_up(91)}.to raise_error error
+  end
 end
